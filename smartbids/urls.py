63509e7bcd6d260ef118f439758811d78c395591
deralexander <nia.munoz@duocuc.cl>
2026-09-10 12:45:34 -0300

    cambio de fuardado en firestore a posgres en registro y inicio de sesion, arreglo de token unico, adaptacion dela base de datos con el perfil

--- smartbids/urls.py
+++ smartbids/urls.py
@@ -12,12 +12,21 @@ urlpatterns = [
     path('404/', views.handler404_view, name='handler404'),
     path('informacion/', views.informacion_view, name='informacion'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('mis-licitaciones/', views.mis_licitaciones_view, name='mis_licitaciones'),
+
     path('api/alertas-activas/', obtener_alertas_activas, name='alertas_activas'),
     path('api/mensajeria/', views.listar_crear_mensajes, name='api_mensajeria'),
     path('api/mensajeria/<int:id>/', views.detalle_mensaje, name='api_detalle_mensaje'),
     path('api/alertas-activas/', views.obtener_alertas_activas, name='api_alertas_activas'),
-    path('mis-licitaciones/', views.mis_licitaciones_view, name='mis_licitaciones'),
     path('api/enviar-correo-bienvenida/', views.enviar_correo_bienvenida, name='enviar_correo_bienvenida'),
     path('api/enviar-codigo-login/', views.enviar_codigo_login, name='enviar_codigo_login'),
     path('api/enviar-correo-cambio-password/', views.enviar_correo_cambio_password, name='enviar_correo_cambio_password'),
+    path('api/validar-codigo-login/', views.validar_codigo_login, name='validar_codigo_login'),
+    path('api/registrar-prospecto/', views.registrar_prospecto, name='registrar_prospecto'),
+    path('api/verificar-sesion/', views.verificar_sesion_activa, name='verificar_sesion'),
+    path('api/obtener-perfil/', views.obtener_perfil_suscriptor, name='obtener_perfil'),
+    path('api/actualizar-perfil/', views.actualizar_perfil_suscriptor, name='actualizar_perfil'),
+    path('api/actualizar-empresa/', views.actualizar_empresa_suscriptor, name='actualizar_empresa'),
+    path('api/actualizar-preferencias/', views.actualizar_preferencias_suscriptor, name='actualizar_preferencias'),
+
 ]
